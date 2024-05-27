12b750e201c7584bdee96bf9b1cf0e9cf8567440
minhyeong112 <miguelnieves556@gmail.com>
2024-05-26 18:08:41 -0700

    v4.0

--- scripts.js
+++ scripts.js
@@ -22,11 +22,3 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
   
-  // Dark mode toggle functionality
-  const darkModeToggle = document.getElementById('dark-mode-toggle');
-  darkModeToggle.addEventListener('click', toggleDarkMode);
-  
-  function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-  }
-  
