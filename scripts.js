330d3caa2f77a2279150950019a15af13f0dbad3
minhyeong112 <miguelnieves556@gmail.com>
2024-06-08 04:50:08 -0700

    v7

--- scripts.js
+++ scripts.js
@@ -1,31 +1,23 @@
-function toggleDarkMode() {
-  document.body.classList.toggle("dark-mode");
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-  // Handle dark mode toggle
-  const homeIconCircle = document.getElementById("home-icon-circle");
-  homeIconCircle.addEventListener("click", toggleDarkMode);
-
   // Handle collapsible sections
   var coll = document.getElementsByClassName("collapsible");
 
   // Reset all content display to none initially (optional)
   for (var i = 0; i < coll.length; i++) {
-      coll[i].nextElementSibling.style.display = "none";
+    coll[i].nextElementSibling.style.display = "none";
   }
 
   for (var i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("focus", function() {
-          this.classList.add("hover"); // Add hover class on focus
-      });
-      coll[i].addEventListener("blur", function() {
-          this.classList.remove("hover"); // Remove hover class on blur
-      });
-      coll[i].addEventListener("click", function() {
-          this.classList.toggle("active");
-          var content = this.nextElementSibling;
-          content.style.display = content.style.display === "block" ? "none" : "block";
-      });
+    coll[i].addEventListener("focus", function() {
+      this.classList.add("hover"); // Add hover class on focus
+    });
+    coll[i].addEventListener("blur", function() {
+      this.classList.remove("hover"); // Remove hover class on blur
+    });
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      content.style.display = content.style.display === "block" ? "none" : "block";
+    });
   }
 });
