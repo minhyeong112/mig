dbe2b90b38b3aea979575087ad9df4a574b123e3
minhyeong112 <miguelnieves556@gmail.com>
2024-06-18 08:29:40 -0700

    v9

--- scripts.js
+++ scripts.js
@@ -21,3 +21,10 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  darkModeToggle.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+  });
+});
