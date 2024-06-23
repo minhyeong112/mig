075929428c41845f5385402db5a6deb725d4ac0d
minhyeong112 <miguelnieves556@gmail.com>
2024-06-23 08:24:16 -0700

    v15

--- scripts.js
+++ scripts.js
@@ -36,3 +36,17 @@ document.addEventListener("DOMContentLoaded", function() {
     localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));
   });
 });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.querySelectorAll(".slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 10000); // Change image every 10 seconds
+}
